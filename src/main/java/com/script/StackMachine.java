d57166783873249342543cc52432d129100d61cc
Jonacofino <cofinojonathan1@gmail.com>
2026-02-13 23:19:01 -0600

    stackmachine

---
+++ src/main/java/com/script/StackMachine.java
@@ -0,0 +1,43 @@
+package com.script;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class StackMachine {
+
+    private Deque<byte[]> stack = new ArrayDeque<>();
+
+    public void push(byte[] data) {
+        stack.push(data);
+    }
+
+    public byte[] pop() {
+        if (stack.isEmpty()) {
+            throw new RuntimeException("Stack underflow");
+        }
+        return stack.pop();
+    }
+
+    public byte[] peek() {
+        if (stack.isEmpty()) {
+            throw new RuntimeException("Stack empty");
+        }
+        return stack.peek();
+    }
+
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+
+    public void printStack() {
+        System.out.print("STACK: ");
+        for (byte[] item : stack) {
+            if (item.length == 1) {
+                System.out.print(item[0] + " ");
+            } else {
+                System.out.print(new String(item) + " ");
+            }
+        }
+        System.out.println();
+    }
+}

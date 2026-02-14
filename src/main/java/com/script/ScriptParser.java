9498939cf8603cf30f5aa8fde6cb0d75fe453483
Jonacofino <cofinojonathan1@gmail.com>
2026-02-13 23:18:13 -0600

    scriptParser

---
+++ src/main/java/com/script/ScriptParser.java
@@ -0,0 +1,11 @@
+package com.script;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ScriptParser {
+
+    public static List<String> parse(String script) {
+        return Arrays.asList(script.trim().split("\\s+"));
+    }
+}
